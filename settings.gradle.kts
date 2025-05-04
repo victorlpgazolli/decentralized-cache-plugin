60eb85a7bf2bc76539277e40923135476a8ce245
victorlpgazolli <victorlpgazolli@gmail.com>
2025-05-03 23:42:59 -0300

    ref: enhance dev ux

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,12 +19,15 @@ rootProject.name = "decentralized-cache-plugin"
 plugins {
     id("dev.victorlpgazolli.decentralized-cache-plugin") version "1.0.0"
 }
-
 buildCache {
     local { isEnabled = false }
 
-    remote<dev.victorlpgazolli.DecentralizedConfiguration> {
-        isEnabled = true
-        isPush    = true
+    val isRecursive = System.getenv("DECENTRALIZED_CACHE_ENABLED")?.toBoolean() ?: false
+    if(isRecursive) {
+        remote<dev.victorlpgazolli.DecentralizedConfiguration> {
+            isEnabled = true
+            isPush    = true
+            baseIpns = "/ipns/gradle.victorlpgazolli.dev"
+        }
     }
 }

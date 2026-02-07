dd55e664f7298e5c89b9dcc5fdac5aa9004db7b3
victorlpgazolli <victorlpgazolli@gmail.com>
2026-02-07 14:50:33 -0300

    add sample

---
+++ sample/settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+plugins {
+    id("dev.victorlpgazolli.decentralized-cache-plugin") version "1.0.0"
+}
+
+buildCache {
+    local { isEnabled = false }
+
+    remote<dev.victorlpgazolli.DecentralizedConfiguration> {
+        isEnabled = true
+        isPush    = true
+        baseIpns = "/ipns/gradle.victorlpgazolli.dev"
+    }
+}
+
+rootProject.name = "sample"

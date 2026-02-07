eee9ca1975410895f53827a6476d1e7015613f70
victorlpgazolli <victorlpgazolli@gmail.com>
2026-02-07 14:49:47 -0300

    rename root folder

---
+++ decentralized-cache-plugin/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    `java-gradle-plugin`
+    `maven-publish`
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.serialization)
+}
+
+val projectGroupId = "dev.victorlpgazolli.decentralized-cache-plugin"
+val versionNumber = "1.0.0"
+
+group = "dev.victorlpgazolli"
+version = versionNumber
+
+
+gradlePlugin {
+    plugins {
+        create("decentralizedCachePlugin") {
+            id = projectGroupId
+            group = projectGroupId
+            implementationClass = "dev.victorlpgazolli.DecentralizedCachePlugin"
+            version = versionNumber
+            tags.set(listOf("cache", "ipfs", "gradle", "distributed"))
+            displayName = "Decentralized Gradle Build Cache (IPFS)"
+            description = "Gradle build cache backed by IPFS"
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(libs.ipfs)
+    implementation(libs.novaCrypto)
+    implementation(libs.kotlinx.serialization)
+}
